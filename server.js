0545019c2c53ee573c1a80d8d83706713df6d395
Raj Yadav <84654548+ItsRaJYadav@users.noreply.github.com>
2023-03-15 22:02:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use("/api/orders", require("./routes/orderRoutes"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/client/build")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
   });
 } else {
   app.get("/", (req, res) => {
