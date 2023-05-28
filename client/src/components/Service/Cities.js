cfdb7b35882306acba5d0f8e2da21a809c332dbb
Raj <raj.yaduvanshi205@gmail.com>
2023-05-28 21:25:06 +0530

    city

--- client/src/components/Service/Cities.js
+++ client/src/components/Service/Cities.js
@@ -24,9 +24,17 @@ const cities = [
       name: 'Kolkata',
       image: 'https://images.unsplash.com/photo-1536421469767-80559bb6f5e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8a29sa2F0YXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
     },{
-        id: 5,
+        id: 6,
+        name: 'Patna',
+        image: 'https://media.istockphoto.com/id/1393991206/photo/buddha-stupa-with-bright-blue-sky-at-morning-from-different-angle.jpg?s=612x612&w=0&k=20&c=K9KborD2LKUXO8Pl9enpJMklQcNMQ6CuojAhYj50nFU='
+      },{
+        id: 7,
+        name: 'Gurugaon',
+        image: 'https://i.ytimg.com/vi/h4EEmCa1hEI/maxresdefault.jpg'
+      },{
+        id: 8,
         name: 'Faridabad',
-        image: 'https://images.unsplash.com/photo-1536421469767-80559bb6f5e1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8a29sa2F0YXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+        image: 'https://www.wearegurgaon.com/wp-content/uploads/2022/09/world-street-faridabad.jpg'
       }
   ];
   
